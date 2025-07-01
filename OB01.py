0989997de2385ea15d0227df9d3126f24d494c2f
AlexanderChernikov83 <Cahe4ka2@ya.ru>
2025-07-01 18:11:14 +0300

    Update OB01.py
    добавлен Шаг2

--- OB01.py
+++ OB01.py
@@ -32,4 +32,28 @@ class TaskManager:
             if not task.completed:
                 print(task)
 
-import datetime
+
+#Шаг 2
+
+class Store:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+        self.items = {}
+
+    def add_item(self, name, price):
+        self.items[name] = price
+
+    def remove_item(self, name):
+        if name in self.items:
+            del self.items[name]
+
+    def get_price(self, name):
+        return self.items.get(name)
+
+    def update_price(self, name, new_price):
+        if name in self.items:
+            self.items[name] = new_price
+
+    def __str__(self):
+        return f"{self.name} — {self.address}"
